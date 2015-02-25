068b78231b55d53c14b2d2cb1d189c59804eba70
Al-Kanor <mathieu.bedu@hotmail.fr>
2015-02-25 14:52:16 +0100

    Amélioration du spawn des ennemis

---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+    #region Attributs publics
+    public float rate = 1;
+    public int proba = 10;    // [proba]% de chance de spawn un ennemi toutes les [rate] secondes
+    public GameObject enemyPrefab;
+    #endregion
+
+    #region Singleton
+    static SpawnManager m_instance;
+    static public SpawnManager instance { get { return m_instance; } }
+    void Awake () {
+        if (null == instance) {
+            m_instance = this;
+        }
+        DontDestroyOnLoad (this);
+    }
+    #endregion
+
+    #region Méthodes privées
+    void FixedUpdate () {
+        
+    }
+
+    IEnumerator SpawnEnemy () {
+        do {
+            int rand = Random.Range (0, 100);
+            if (rand < proba) {
+                float x, z;
+                /*
+                 * Coordonnées de spawn (Carte vue du ciel, abscisses = x, ordonnées = z) :
+                 * -100, [-100, 100] => Colonne de gauche
+                 * 100, [-100, 100] => Colonne de droite
+                 * [-100, 100], -100 => Ligne du bas
+                 * [-100, 100], 100 => Ligne du haut
+                 */
+                if (0 == Random.Range (0, 2)) {
+                    // 1 chance sur 2 de spawn sur une colonne
+                    x = 0 == Random.Range (0, 2) ? -100 : 100;  // Colonne gauche / droite
+                    z = Random.Range (-100, 100);
+                }
+                else {
+                    // 1 chance sur 2 de spawn sur une ligne
+                    x = Random.Range (-100, 100);
+                    z = 0 == Random.Range (0, 2) ? -100 : 100;  // Ligne haut / bas
+                }
+
+                Instantiate (enemyPrefab, new Vector3(x, 0, z), Quaternion.identity);
+            }
+            yield return new WaitForSeconds (rate);
+        } while (true);
+    }
+
+    void Start () {
+        StartCoroutine ("SpawnEnemy");
+    }
+    #endregion
+}

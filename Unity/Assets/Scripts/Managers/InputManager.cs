5d95945440c89b9fc4ce42730f4e6eb6a78498c2
Al-Kanor <mathieu.bedu@hotmail.fr>
2015-02-25 17:16:07 +0100

    Optimisation générale

---
+++ Unity/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+    #region Méthodes privées
+    void FixedUpdate () {
+        if (Input.GetKeyDown (KeyCode.Escape)) {
+            Application.Quit ();
+        }
+    }
+    #endregion
+}
